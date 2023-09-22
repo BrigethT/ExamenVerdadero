ece20e08ff606cf735f1edd9b18e6caa89ce1d8c
BrigethT <brigeth.toapanta@epn.edu.ec>
2023-09-21 21:57:51 -0500

    x

---
+++ Facu Decano/Facu Decano/EntidadesMateria/Decano.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesMateria
+{
+    public class Decano
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+
+        public Decano() { }
+        public Decano(int idnew, string nombre, string apellido)
+        {
+            this.Apellido = apellido;
+            this.Nombre = nombre;
+            this.Id = idnew;
+        }
+
+        public override string ToString()
+        {
+            return Nombre + " " + Apellido;
+        }
+    }
+}
+
---
+++ Facu Decano/Facu Decano/EntidadesMateria/Facultad.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadesMateria
+{
+
+    public class Facultad
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Id_Decano { get; set; }
+
+        public Facultad(int idfac, string nombre, int iddec)
+        {
+            this.Id = idfac;
+            this.Nombre = nombre;
+            this.Id_Decano = iddec;
+        }
+        public Facultad() { }
+
+        public override string ToString()
+        {
+            return Nombre;
+        }
+    }
+
+}
